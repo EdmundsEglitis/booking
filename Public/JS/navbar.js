6c8ccf56df4ca596a6ad25dc82e2090ac049cc28
EdmundsEglitis <126756186+EdmundsEglitis@users.noreply.github.com>
2024-06-12 14:07:30 +0300

    Delete Public directory
    erswgd

--- Public/JS/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggler = document.getElementById("navbar-toggler");
-    const menu = document.getElementById("navbar-menu");
-
-    toggler.addEventListener("click", function() {
-        menu.classList.toggle("active");
-    });
-});
--- Public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require "../app/Core/Session.php";
-require "../app/Core/Validator.php";
-require "../app/Core/router.php";
